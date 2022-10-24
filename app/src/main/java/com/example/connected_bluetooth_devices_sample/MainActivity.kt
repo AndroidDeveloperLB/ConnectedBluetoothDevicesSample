ba69875948f2a43fb4fbf92164fc195ba0397cf7
AndroidDeveloperLB <lblb636@gmail.com>
2022-10-24 10:29:57 +0300

    clean code

--- app/src/main/java/com/example/connected_bluetooth_devices_sample/MainActivity.kt
+++ app/src/main/java/com/example/connected_bluetooth_devices_sample/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        viewModel.initWithLifecycle(lifecycle)
         viewModel.bluetoothTurnedOnLiveData.observe(this) {
             Log.d("AppLog", "MainActivity bluetoothTurnedOnLiveData BT turned on? $it")
         }
@@ -29,71 +30,5 @@ class MainActivity : AppCompatActivity() {
                 requestPermissions(arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 1)
             }
         }
-        //        viewModel.connectedDevicesLiveData.observe(this) { state ->
-        //            when (state) {
-        //                MainActivityViewModel.ConnectedDevicesState.BluetoothIsTurnedOff -> {
-        //                    Log.d("AppLog", "disconnected")
-        //                }
-        //
-        //                is MainActivityViewModel.ConnectedDevicesState.GotResult -> {
-        //                    val connectedDevices = state.connectedDevices
-        //                    if(connectedDevices.isEmpty())
-        //                        Log.d("AppLog", "no connected devices")
-        //                    else {
-        //                        //noinspection MissingPermission
-        //                        val connectedDevicesNames = connectedDevices.joinToString { it.name }
-        //                        Log.d("AppLog", "connected devices:$connectedDevicesNames")
-        //                    }
-        //                }
-        //
-        //                MainActivityViewModel.ConnectedDevicesState.Idle ->{
-        //                    Log.d("AppLog", "idle")
-        //                }
-        //                MainActivityViewModel.ConnectedDevicesState.Loading -> {
-        //                    Log.d("AppLog", "checking connected devices")
-        //                }
-        //                MainActivityViewModel.ConnectedDevicesState.NeedBlueToothConnectPermission -> {
-        //                    Log.d("AppLog", "NeedBlueToothConnectPermission")
-        //                }
-        //            }
-        //        }
-        //        val bluetoothAdapter: BluetoothAdapter =
-        //            getSystemService<BluetoothManager>().adapter
-        //
-        //        val btFoundReceiver = object : BroadcastReceiver() {
-        //            override fun onReceive(context: Context?, intent: Intent) {
-        //                val action = intent.action
-        //                Log.d("AppLog", "action:$action")
-        //                if (ActivityCompat.checkSelfPermission(this@MainActivity, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
-        //                    if (action == BluetoothDevice.ACTION_FOUND) {
-        //                        val device: BluetoothDevice =
-        //                            intent.getParcelableExtraCompat(BluetoothDevice.EXTRA_DEVICE)!!
-        //                        val clazz: BluetoothClass =
-        //                            intent.getParcelableExtraCompat(BluetoothDevice.EXTRA_CLASS)!!
-        //                        Log.d("AppLog", "found BT of device:${device.name} ${device.address} ${clazz.majorDeviceClass} ${clazz.deviceClass}")
-        //                    }
-        //                }
-        //                else{
-        //                    Log.d("AppLog", "missing permission to get found BT device")
-        //                }
-        //
-        //            }
-        //
-        //        }
-        //        registerReceiver(btFoundReceiver, IntentFilter().also {
-        //            it.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED)
-        //            it.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)
-        //            it.addAction(BluetoothDevice.ACTION_FOUND)
-        //        })
-        //        val successCancelingDiscovery = bluetoothAdapter.cancelDiscovery()
-        //        Log.d("AppLog", "successCancelingDiscovery?$successCancelingDiscovery")
-        //        val successStartingDiscovery = bluetoothAdapter.startDiscovery()
-        //        Log.d("AppLog", "successStartingDiscovery?$successStartingDiscovery")
-        //        lifecycle.addObserver(object : DefaultLifecycleObserver {
-        //            override fun onDestroy(owner: LifecycleOwner) {
-        //                super.onDestroy(owner)
-        //                unregisterReceiver(btFoundReceiver)
-        //            }
-        //        })
     }
 }
--- app/src/main/java/com/example/connected_bluetooth_devices_sample/MainActivityViewModel.kt
+++ app/src/main/java/com/example/connected_bluetooth_devices_sample/MainActivityViewModel.kt
@@ -8,17 +8,63 @@ import android.content.pm.PackageManager
 import android.util.Log
 import androidx.annotation.UiThread
 import androidx.core.app.ActivityCompat
+import androidx.core.content.getSystemService
 import androidx.lifecycle.*
 
 @UiThread
 class MainActivityViewModel(application: Application) : BaseViewModel(application) {
     private val bluetoothAdapter: BluetoothAdapter =
-        context.getSystemService<BluetoothManager>().adapter
+        context.getSystemService<BluetoothManager>()!!.adapter
     private var bluetoothHeadsetProfile: BluetoothProfile? = null
     val connectedDevicesLiveData =
         DistinctLiveDataWrapper(MutableLiveData<ConnectedDevicesState>(ConnectedDevicesState.Idle))
     val bluetoothTurnedOnLiveData = DistinctLiveDataWrapper(MutableLiveData<Boolean?>(null))
     val isConnectedToBtHeadsetLiveData = DistinctLiveDataWrapper(MutableLiveData<Boolean?>(null))
+    private val pollingBtStateRunnable: Runnable
+
+    init {
+        updateBtStates()
+        pollingBtStateRunnable = object : Runnable {
+            override fun run() {
+                updateBtStates()
+                handler.postDelayed(this, POLLING_TIME_IN_MS)
+            }
+        }
+        // Establish connection to the proxy.
+        val serviceListener = object : BluetoothProfile.ServiceListener {
+            override fun onServiceConnected(profile: Int, bluetoothProfile: BluetoothProfile) {
+                this@MainActivityViewModel.bluetoothHeadsetProfile = bluetoothProfile
+                handler.removeCallbacks(pollingBtStateRunnable)
+                pollingBtStateRunnable.run()
+            }
+
+            override fun onServiceDisconnected(profile: Int) {
+                handler.removeCallbacks(pollingBtStateRunnable)
+                updateBtStates()
+            }
+        }
+        bluetoothAdapter.getProfileProxy(context, serviceListener, BluetoothProfile.HEADSET)
+        onClearedListeners.add {
+            this.bluetoothHeadsetProfile?.let { bluetoothProfile ->
+                bluetoothAdapter.closeProfileProxy(BluetoothProfile.HEADSET, bluetoothProfile)
+            }
+            handler.removeCallbacks(pollingBtStateRunnable)
+        }
+    }
+
+    fun initWithLifecycle(lifecycle: Lifecycle) {
+        lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onResume(owner: LifecycleOwner) {
+                super.onResume(owner)
+                pollingBtStateRunnable.run()
+            }
+
+            override fun onPause(owner: LifecycleOwner) {
+                super.onPause(owner)
+                handler.removeCallbacks(pollingBtStateRunnable)
+            }
+        })
+    }
 
     @UiThread
     private fun updateBtStates() {
@@ -54,187 +100,7 @@ class MainActivityViewModel(application: Application) : BaseViewModel(applicatio
         }
     }
 
-    init {
-        //        Log.d("AppLog", "MainActivityViewModel CTOR")
-        Log.d("AppLog", "bluetoothAdapter.state:${getBluetoothAdapterStateStr(bluetoothAdapter.state)} ${checkIfConnectedToHeadset(bluetoothAdapter)}")
-        updateBtStates()
-        val pollingBtStateRunnable = object : Runnable {
-            override fun run() {
-                updateBtStates()
-                handler.postDelayed(this, POLLING_TIME_IN_MS)
-            }
-        }
-        // Establish connection to the proxy.
-        val serviceListener = object : BluetoothProfile.ServiceListener {
-            override fun onServiceConnected(profile: Int, bluetoothProfile: BluetoothProfile) {
-                Log.d("AppLog", "onServiceConnected  bluetoothAdapter.state:${getBluetoothAdapterStateStr(bluetoothAdapter.state)} ${checkIfConnectedToHeadset(bluetoothAdapter)}")
-                this@MainActivityViewModel.bluetoothHeadsetProfile = bluetoothProfile
-                handler.removeCallbacks(pollingBtStateRunnable)
-                pollingBtStateRunnable.run()
-                //                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
-                //                    val connectedDevices = bluetoothProfile.connectedDevices
-                //                    Log.d("AppLog", "connectedDevices:${connectedDevices.size} ${connectedDevices.joinToString { it.name }}")
-                //                    connectedDevicesLiveData.value =
-                //                        ConnectedDevicesState.GotResult(connectedDevices.toHashSet())
-                //                } else {
-                //                    //                    Log.d("AppLog", "missing permission to get information of connected devices")
-                //                    connectedDevicesLiveData.value =
-                //                        ConnectedDevicesState.NeedBlueToothConnectPermission
-                //                }
-                //                onClearedListeners.add {
-                //                    this@MainActivityViewModel.bluetoothProfile = null
-                //                    //                    Log.d("AppLog", "closing bluetoothAdapter")
-                //                    bluetoothAdapter.closeProfileProxy(BluetoothProfile.HEADSET, bluetoothProfile)
-                //                }
-            }
-
-            override fun onServiceDisconnected(profile: Int) {
-                Log.d("AppLog", "onServiceDisconnected bluetoothAdapter.state:${getBluetoothAdapterStateStr(bluetoothAdapter.state)}  ${checkIfConnectedToHeadset(bluetoothAdapter)}")
-                handler.removeCallbacks(pollingBtStateRunnable)
-                updateBtStates()
-                //                connectedDevicesLiveData.value = ConnectedDevicesState.BluetoothIsTurnedOff
-            }
-        }
-        bluetoothAdapter.getProfileProxy(context, serviceListener, BluetoothProfile.HEADSET)
-        onClearedListeners.add {
-            this.bluetoothHeadsetProfile?.let { bluetoothProfile ->
-                bluetoothAdapter.closeProfileProxy(BluetoothProfile.HEADSET, bluetoothProfile)
-            }
-            handler.removeCallbacks(pollingBtStateRunnable)
-        }
-
-        //
-        //        val btConnectedReceiver = object : BroadcastReceiver() {
-        //            override fun onReceive(context: Context, intent: Intent) {
-        //                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-        //                    Log.d("AppLog", "missing permission to handle BT connected devices intent")
-        //                    connectedDevicesLiveData.value =
-        //                        ConnectedDevicesState.NeedBlueToothConnectPermission
-        //                    return
-        //                }
-        //                val action = intent.action
-        //                val bluetoothDevice: BluetoothDevice =
-        //                    intent.getParcelableExtraCompat(BluetoothDevice.EXTRA_DEVICE)!!
-        //                val bondState = bluetoothDevice.bondState
-        //                val connectionState = bluetoothProfile?.getConnectionState(bluetoothDevice)
-        //                when (action) {
-        //                    BluetoothDevice.ACTION_ACL_CONNECTED -> {
-        //                        Log.d("AppLog", "connected device:${bluetoothDevice.name} bondState:${getBondStateStr(bondState)} connectionState:${getBluetoothDeviceConnectionStateStr(connectionState)}")
-        //                        if (bondState == BluetoothDevice.BOND_NONE || connectionState == BluetoothProfile.STATE_DISCONNECTING || connectionState == BluetoothProfile.STATE_DISCONNECTED)
-        //                            return
-        //                        (connectedDevicesLiveData.value as? ConnectedDevicesState.GotResult)?.let { result: ConnectedDevicesState.GotResult ->
-        //                            val newConnectedDevicesCollection =
-        //                                HashSet<BluetoothDevice>(result.connectedDevices.size + 1)
-        //                            newConnectedDevicesCollection.addAll(result.connectedDevices)
-        //                            newConnectedDevicesCollection.add(bluetoothDevice)
-        //                            connectedDevicesLiveData.value =
-        //                                ConnectedDevicesState.GotResult(newConnectedDevicesCollection)
-        //                        }
-        //                    }
-        //
-        //                    BluetoothDevice.ACTION_ACL_DISCONNECTED -> {
-        //                        Log.d("AppLog", "disconnected device:${bluetoothDevice.name} bondState:${getBondStateStr(bondState)} connectionState:${getBluetoothDeviceConnectionStateStr(connectionState)}")
-        //                        (connectedDevicesLiveData.value as? ConnectedDevicesState.GotResult)?.let { result: ConnectedDevicesState.GotResult ->
-        //                            val newConnectedDevicesCollection =
-        //                                HashSet<BluetoothDevice>(result.connectedDevices.size + 1)
-        //                            newConnectedDevicesCollection.addAll(result.connectedDevices)
-        //                            newConnectedDevicesCollection.remove(bluetoothDevice)
-        //                            connectedDevicesLiveData.value =
-        //                                ConnectedDevicesState.GotResult(newConnectedDevicesCollection)
-        //                        }
-        //                    }
-        //
-        //                    BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED -> {
-        //                        Log.d("AppLog", "BT connection changed: ${getBluetoothAdapterStateStr(bluetoothAdapter.state)}")
-        //                    }
-        //
-        //                    else -> {
-        //                        //                        Log.d("AppLog", "btConnectedReceiver action:$action")
-        //                    }
-        //                }
-        //            }
-        //
-        //        }
-        //        context.registerReceiver(btConnectedReceiver, IntentFilter().also { filter ->
-        //            //                            filter.addAction(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)
-        //            filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED)
-        //            filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED)
-        //            filter.addAction(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED)
-        //        })
-        //        onClearedListeners.add {
-        //            //            Log.d("AppLog", "closing btConnectedReceiver")
-        //            context.unregisterReceiver(btConnectedReceiver)
-        //        }
-    }
-
-
-    //    fun refresh() {
-    //        connectedDevicesLiveData.value = ConnectedDevicesState.Loading
-    //        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
-    //            //            Log.d("AppLog", "missing permission to get connected devices list")
-    //            connectedDevicesLiveData.value = ConnectedDevicesState.NeedBlueToothConnectPermission
-    //        } else {
-    //            bluetoothProfile.let { bluetoothProfile ->
-    //                if (bluetoothProfile != null) {
-    //                    connectedDevicesLiveData.value =
-    //                        ConnectedDevicesState.GotResult(bluetoothProfile.connectedDevices.toHashSet())
-    //                } else {
-    //                    connectedDevicesLiveData.value = ConnectedDevicesState.BluetoothIsTurnedOff
-    //                }
-    //            }
-    //        }
-    //    }
-
     companion object {
         private const val POLLING_TIME_IN_MS = 500L
-        private fun getBluetoothAdapterStateStr(bluetoothAdapterState: Int): String {
-            //            https://developer.android.com/reference/android/bluetooth/BluetoothAdapter#getState()
-            return when (bluetoothAdapterState) {
-                BluetoothAdapter.STATE_OFF -> "STATE_OFF ($bluetoothAdapterState)"
-                BluetoothAdapter.STATE_TURNING_ON -> "STATE_TURNING_ON ($bluetoothAdapterState)"
-                BluetoothAdapter.STATE_ON -> "STATE_ON ($bluetoothAdapterState)"
-                BluetoothAdapter.STATE_TURNING_OFF -> "STATE_TURNING_OFF ($bluetoothAdapterState)"
-                else -> "unknownBluetoothAdapterState:$bluetoothAdapterState"
-            }
-        }
-
-        private fun getBondStateStr(bondState: Int): String {
-            //            https://developer.android.com/reference/android/bluetooth/BluetoothDevice#getBondState()
-            return when (bondState) {
-                BluetoothDevice.BOND_BONDED -> "BOND_BONDED ($bondState)"
-                BluetoothDevice.BOND_BONDING -> "BOND_BONDING ($bondState)"
-                BluetoothDevice.BOND_NONE -> "BOND_NONE ($bondState)"
-                else -> "unknownBondState:$bondState"
-            }
-        }
-
-        @Suppress("KotlinConstantConditions")
-        private fun getBluetoothDeviceConnectionStateStr(connectionState: Int?): String {
-            //            https://developer.android.com/reference/android/bluetooth/BluetoothProfile#getConnectionState(android.bluetooth.BluetoothDevice)
-            return when (connectionState) {
-                BluetoothProfile.STATE_CONNECTED -> "STATE_CONNECTED ($connectionState)"
-                BluetoothProfile.STATE_CONNECTING -> "STATE_CONNECTING ($connectionState)"
-                BluetoothProfile.STATE_DISCONNECTING -> "STATE_DISCONNECTING ($connectionState)"
-                BluetoothProfile.STATE_DISCONNECTED -> "STATE_DISCONNECTED ($connectionState)"
-                null -> "unknown"
-                else -> "unknownConnectionState:$connectionState"
-            }
-        }
-
-        private fun checkIfConnectedToHeadset(bluetoothAdapter: BluetoothAdapter): String {
-            try {
-                val str = when (val headsetConnectionState =
-                    bluetoothAdapter.getProfileConnectionState(BluetoothProfile.HEADSET)) {
-                    BluetoothAdapter.STATE_DISCONNECTED -> "STATE_DISCONNECTED ($headsetConnectionState)"
-                    BluetoothAdapter.STATE_CONNECTING -> "STATE_CONNECTING ($headsetConnectionState)"
-                    BluetoothAdapter.STATE_CONNECTED -> "STATE_CONNECTED ($headsetConnectionState)"
-                    BluetoothAdapter.STATE_DISCONNECTING -> "STATE_DISCONNECTING ($headsetConnectionState)"
-                    else -> "unknownHeadsetState:headsetConnectionState"
-                }
-                return "checkIfConnectedToHeadset:$str"
-            } catch (e: SecurityException) {
-                return "failed to get headsetConnectionState:$e"
-            }
-        }
     }
 }
